e7ca52dbeb217d55ae9103cdb129b87bcb3f3cec
AkiShiroshita <akihirokun8@gmail.com>
2021-10-12 12:07:51 +0900

    add a gitignore

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
 # Empyema-project
 
-This is R scripts for our "Empyema project". This folder contains scripts for data cleaning and statistical analysis.
+This is a project folder for our "Empyema project". This folder contains scripts for data cleaning and statistical analysis.
---
+++ analysis/main_analysis.R
@@ -0,0 +1,16 @@
+
+# Set-up ------------------------------------------------------------------
+
+library("readr")
+library("readxl")
+library("tidyverse")
+library("lubridate")
+library("tidylog")
+library("ggplot2")
+library("ggthemes")
+library("ggplotgui")
+library("arsenal")
+
+# Data import -------------------------------------------------------------
+
+
